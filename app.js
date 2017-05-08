9d200b7cb27ca460c4a1204a0995ab38b87a4d2b
j8298c <juliocmojica@gmail.com>
2017-05-08 14:30:47 -0400

    fixing css

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+require('dotenv').config()
 const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
--- test/config.js
+++ test/config.js
@@ -1,6 +1,5 @@
 
-exports.APOLLO_PRODUCTION_DATABASE = process.env.APOLLO_PRODUCTION_DATABASE ||
-                       global.DATABASE_URL;
+exports.APOLLO_PRODUCTION_DATABASE = 'mongodb://root:root@ds111529.mlab.com:11529/apollo';
                       
 exports.APOLLO_TEST_DATABASE = 
 	        'mongodb://root:root@ds115701.mlab.com:15701/apollotest';
