f2a15471b801a74986007cea66db51b92468f3ac
j8298c <juliocmojica@gmail.com>
2017-06-05 23:10:25 -0400

    fixing routes for crud

--- routes/index.js
+++ routes/index.js
@@ -1,58 +1,102 @@
 module.exports = (app, Workout) =>{
-        
-  app.get('/', (req, res)=>{
-    res.render('index', {title: 'Apollo'});
-  });
 
-  app.get('/all', (req, res, next)=>{
-      Workout.find({})
-      .exec((err, workouts)=>{
-          if(err){
-              console.log(err);
-          } else {
-          console.log(workouts, 'workouts in the DB');
-        res.render('allworkouts', {workouts: workouts})
-          }
-      })
-  });
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'Apollo'
+    });
+});
+
+app.get('/all', (req, res, next) => {
+    Workout.find({})
+        .exec((err, workouts) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(workouts, 'workouts in the DB');
+                res.render('allworkouts', {
+                    workouts: workouts
+                })
+            }
+        })
+});
 
 //create workout routes
-  app.get('/create', (req, res, next)=>{
-      res.render('createworkout')
-  })
-  app.post('/create', (req, res, next)=>{
-      const workout = new Workout({
-          name: req.body.name,
-          reps: req.body.reps,
-          sets: req.body.sets
-      });
-      workout.save(err=>{
-          if(err){
-              console.log(err);
-          } else {
+app.get('/create', (req, res, next) => {
+    res.render('createworkout')
+})
+app.post('/create', (req, res, next) => {
+    const workout = new Workout({
+        name: req.body.name,
+        reps: req.body.reps,
+        sets: req.body.sets
+    });
+    workout.save(err => {
+        if (err) {
+            console.log(err);
+        } else {
             Workout.find({})
-             .exec((err, workouts)=>{
-          if(err){
-              console.log(err);
-          } else {
-          console.log(workouts, 'workouts in the DB');
-        res.render('allworkouts', {workouts: workouts})
-          }
-      })
-          }
-      })
-      console.log('the workout', workout);
-  });
+                .exec((err, workouts) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log(workouts, 'workouts in the DB');
+                        res.render('allworkouts', {
+                            workouts: workouts
+                        })
+                    }
+                })
+        }
+    })
+    console.log('the workout', workout);
+});
 
-//edit workout routes
-    app.get('/workout/:name', (req, res, next)=>{
-      Workout.findOne({name: req.params.name}, (err, workout)=>{
-          if(err){
-              console.log(err);
-          } else {
-              res.render('workout', {name: workout.name, reps: workout.reps, sets: workout.sets})
-          }
-      })
-  });
+app.get('/workout/:name', (req, res, next) => {
+    Workout.findOne({
+        name: req.params.name
+    }, (err, workout) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('workout', {
+                name: workout.name,
+                reps: workout.reps,
+                sets: workout.sets
+            })
+        }
+    })
+});
+//edit workouts
+app.get('/edit/:name', (req, res) => {
+    Workout.findOne({
+        name: req.params.name
+    }, (err, workout) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('editworkout', {
+                name: workout.name,
+                reps: workout.reps,
+                sets: workout.sets
+            });
+        }
+    })
+});
+app.put('/edit', (req, res) => {
+    Workout.findOneAndUpdate({name: req.params.name}, {reps: req.params.reps, sets: req.params.sets}, (err, workout)=>{
+        if (err) throw err;
+
+        console.log(workout);
+    })
+});
+
+app.delete('/delete/:name', (req, res) => {
+    Workout.findOneAndDelete({"name": req.param.name}, (err)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.send('user deleted');
+        }
+    })
+});
 
 }
