64a10ec968f7209b1834dca6fc1cb4913f76f824
j8298c <juliocmojica@gmail.com>
2017-06-08 22:23:29 -0400

    user test completed

--- test/test_user.js
+++ test/test_user.js
@@ -5,13 +5,13 @@ const express = require('express');
 const assert = require('assert');
 const User = require('../models/usermodel');
 const faker = require('faker');
-
+const chaiHttp = require('chai-http');
 const port = process.env.PORT || 8081;
 const testDB = 'mongodb://localhost/apollotesting';
 const should = chai.should();
 const app = express();
-
-//hooks
+chai.use(chaiHttp);
+//hook
 
 //give the database users to use for testing
 function seedData(){
@@ -94,4 +94,77 @@ describe('Testing Api endpoints', ()=>{
             return User.count();
         });
     })
-})
+    describe('User POST request', ()=>{
+        it('should reject a user saving to db when value is incorrect', ()=>{
+            User.count({}, (err, count)=>{
+                return count;
+            })
+            let res;
+            chai.request('http://localhost:8081')
+                .post('/signup', {email: faker.name.findName(), password: faker.lorem.word()})
+                .then(_res =>{
+                    res = _res;
+                    res.should.have.status(406)
+                    assert.equal(count, 4);
+                })
+        })
+        it('should save the user when values are correct', ()=>{
+            let res;
+            chai.request(app)
+            .post('/signup', {email: faker.internet.email(), password: faker.lorem.word()})
+            .then(_res =>{
+                res=_res;
+                res.should.have.status(200);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.should.include.keys('email', 'password');
+                assert.equal(count, 5);
+            })
+        })
+    });
+    describe('User PUT request', ()=>{
+        it('should add new user to the DB for put test', ()=>{
+            let res;
+            chai.request(app)
+            .post('/signup', {email: 'heman@gmail.com', password: faker.lorem.word()})
+        })
+        it('should find created user and edit the password', ()=>{
+            chai.request(app)
+            .put('/edit', {name: 'heman@gmail.com', password: faker.name.lastName()})
+            .then(_res=>{
+                res.should.have.status(204);
+                res.should.be.json;
+                res.should.be.a('object');
+            })
+            console.log('updated');
+            return chai.request(app)
+        })
+    })
+    describe('user Delete request', ()=>{
+        it('should add new user to db for test', ()=>{
+            let res;
+            chai.request(app)
+            .post('/signup', {email: 'deleteme@gmail.com', password: faker.lorem.word()})
+            .then(_res =>{
+                res = _res;
+                res.should.have.status(200);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.should.include.keys('email', 'password');
+                assert.equal(count, 5);
+            })
+        })
+        it('should find newly created user and delete it from DB', ()=>{
+            let res;
+            chai.request(app)
+            .delete('/delete',{email: 'deleteme@gmail.com'})
+            .then(_res =>{
+                res = _res;
+                res.should.have.status(200);
+                assert.equal(count, 4)
+            })
+            console.log('deleted');
+        })
+    })
+    
+});
